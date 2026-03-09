732c09d8b121e4ebd8bbdbe86d2136fc7ddef9e6
Vlad <numaeria@gmail.com>
2026-03-09 12:59:19 +0300

    cделал структуры приватными

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,27 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypRed: UIColor {
+        UIColor(named: "YP Red (iOS)") ?? .red
+    }
+    
+    static var ypGreen: UIColor {
+        UIColor(named: "YP Green (iOS)") ?? .green
+    }
+    
+    static var ypWhite: UIColor {
+        UIColor(named: "YP White (iOS)") ?? .white
+    }
+    
+    static var ypGray: UIColor {
+        UIColor(named: "YP Gray (iOS)") ?? .gray
+    }
+    
+    static var ypBlack: UIColor {
+        UIColor(named: "YP Black (iOS)") ?? .black
+    }
+    
+    static var Background: UIColor {
+        UIColor(named: "YP Background (iOS)") ?? .black
+    }
+}
